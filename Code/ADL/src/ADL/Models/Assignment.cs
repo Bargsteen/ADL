29be72b649053491b42dcd7827d2f3bcf5f617fa
Xin <gigm@live.dk>
2016-11-08 15:55:56 +0100

    Models mappen og Ass <3
    Ibrahim og jeg har tilføjet models mappen samt Assignment klassen

---
+++ Code/ADL/src/ADL/Models/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADL.Models
+{
+    public class Assignment
+    {
+        public string[] assignments { get; set; } 
+        public string headline { get; set; }
+        public string description { get; set; }
+    }
+}
