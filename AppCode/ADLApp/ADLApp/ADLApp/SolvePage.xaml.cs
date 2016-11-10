b94eb792b3bdb721d13247ec105ab9e8fe96449a
Peter Madsen <p-printz@hotmail.com>
2016-11-10 10:18:35 +0100

    added some shit, trying to async GET info from localhost

--- AppCode/ADLApp/ADLApp/ADLApp/SolvePage.xaml.cs
+++ AppCode/ADLApp/ADLApp/ADLApp/SolvePage.xaml.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ADLApp.Model;
 using Xamarin.Forms;
 
 namespace ADLApp
 {
     public partial class SolvePage : ContentPage
     {
-        public SolvePage()
+        public SolvePage(Assignment a)
         {
+            currentAssignment = a;
             InitializeComponent();
         }
+        public Assignment currentAssignment { get; set; }
     }
 }
