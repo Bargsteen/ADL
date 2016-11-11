bafcb75e410abb65a1ebd6220f9525d30ab467d5
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2016-11-11 13:43:09 +0100

    Sqlite and auto migrate

--- WebCode/src/ADL/Startup.cs
+++ WebCode/src/ADL/Startup.cs
@@ -21,21 +21,21 @@ namespace ADL
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //var connectionString = @"Server=tcp:adlearning.database.windows.net,1433;Initial Catalog=assignments;Persist Security Info=False;User ID={adladmin};Password={wqpLMCBE+4G4};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Filename=./ADL.db"));
             services.AddTransient<IAssignmentRepository, EFAssignmentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ApplicationDbContext db)
         {
+            db.Database.Migrate();
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
-            
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
+            app.UseDeveloperExceptionPage();
             
         }
     }
