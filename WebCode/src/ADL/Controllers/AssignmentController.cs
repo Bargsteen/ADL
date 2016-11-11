ecc3ca3733d9822b1c9a99dc56c6e3b8860c105f
Xin <gigm@live.dk>
2016-11-11 14:09:37 +0100

    Revert "created a locationcontroller"
    This reverts commit 9b443417641e88dee9caa162c8b99ca5c08da7c6.

--- WebCode/src/ADL/Controllers/AssignmentController.cs
+++ WebCode/src/ADL/Controllers/AssignmentController.cs
@@ -4,8 +4,13 @@ using System.Linq;
 
 namespace ADL.Controllers
 {
-    public class LocationController : Controller
+    public class AssignmentController : Controller
     {
+        IAssignmentRepository repository;
+        public AssignmentController(IAssignmentRepository repo)
+        {
+            repository = repo;
+        }
         [HttpGet]
         public ViewResult Create()
         {
@@ -14,28 +19,43 @@ namespace ADL.Controllers
         [HttpPost]
         public ViewResult Create(Assignment assignment)
         {
+            if (ModelState.IsValid)
+            {
+                repository.Add(assignment);
+                return View("SuccesfullyCreated");
+            }
             return View();
         }
 
         public ViewResult Delete(int? assignmentId)
         {
-            return View();
+            Assignment assignment = repository.Assignments.FirstOrDefault(a => a.AssignmentID == assignmentId);
+            if(assignment != null)
+            {
+                repository.Delete(assignment);
+            }
+            return View(nameof(List), repository.Assignments);
         }
-
         [HttpGet]
         public ViewResult Edit(int? assignmentId)
         {
-            return View();
+            Assignment assignment = repository.Assignments.FirstOrDefault(a => a.AssignmentID == assignmentId);
+            if(assignment != null)
+            {
+                return View(assignment);
+            }
+            return View(nameof(List), repository.Assignments);
         }
         [HttpPost]
         public ViewResult Edit(Assignment editedAssignment)
         {
-            return View();
+            repository.Edit(editedAssignment);
+            return View("SuccesfullyEdited");
         }
 
         public ViewResult List()
         {
-            return View();
+            return View(repository.Assignments);
         }
         public ViewResult Solve()
         {
