71d2f034cc91994ff7ce8eb6740fdb5f79fe1d3b
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2016-11-11 15:30:59 +0100

    added Location api

--- WebCode/src/ADL/Controllers/ApiController.cs
+++ WebCode/src/ADL/Controllers/ApiController.cs
@@ -2,26 +2,43 @@ using Microsoft.AspNetCore.Mvc;
 using ADL.Models;
 using System.Linq;
 using Newtonsoft.Json;
+using System;
 
 namespace ADL.Controllers
 {
     public class ApiController : Controller
     {
-        private IAssignmentRepository repository;
-        public ApiController(IAssignmentRepository repo)
+        private IAssignmentRepository assignmentRepository;
+        private ILocationRepository locationRepository;
+        private Random random = new Random();
+        public ApiController(IAssignmentRepository assignmentRepo, ILocationRepository locationRepo)
         {
-            repository = repo;
+            assignmentRepository = assignmentRepo;
+            locationRepository = locationRepo;
         }
 
         public string GetAssignment(int? id)
         {
-            Assignment assignment = repository.Assignments.FirstOrDefault(a => a.AssignmentID == id);
+            Assignment assignment = assignmentRepository.Assignments.FirstOrDefault(a => a.AssignmentID == id);
             if(assignment != null)
             {
                 return JsonConvert.SerializeObject(assignment);
             }
-            return "invalid";
+            return "Invalid AssignmentID given.";
         }
-       
+
+        public string Location(int? id)
+        {
+            Location location = locationRepository.Location.FirstOrDefault(l => l.LocationID == id);
+            if(location != null)
+            {
+                Assignment randomAssignment = assignmentRepository.Assignments.ElementAt(random.Next(0, assignmentRepository.Assignments.Count());
+                if(randomAssignment != null)
+                {
+                    return JsonConvert.SerializeObject(randomAssignment);
+                }
+            }
+            return "Location does not exist";
+        }
     }
 }
