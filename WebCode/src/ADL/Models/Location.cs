822136b597e1d9035ebc2165c96dbc2c8b590f1c
Xin <gigm@live.dk>
2016-11-11 14:26:56 +0100

    location and location/add.view created
    ^

---
+++ WebCode/src/ADL/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADL.Models
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+
+        [Required(ErrorMessage = "Venligst indtast en titel")]
+        [MinLengthAttribute(2)]
+        public string Titel { get; set; }
+
+        [Required(ErrorMessage = "Venligst vælg HVOR lokationen er!")]
+        [MinLengthAttribute(2)]
+        public string Description { get; set; }
+    }
+}
