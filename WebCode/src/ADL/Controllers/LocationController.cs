d6018ba0191d508577819dd011b44efad85c5a7f
Xin <gigm@live.dk>
2016-11-15 12:53:05 +0100

    Revert "creatd qrcode generatiion"
    This reverts commit f63bdcb0cad5ac30cf0db0a605e665343424c753.

--- WebCode/src/ADL/Controllers/LocationController.cs
+++ WebCode/src/ADL/Controllers/LocationController.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ADL.Models;
-using QRCoder;
-using System.IO;
-using System.Collections;
-using System;
+using System.Linq;
 
 namespace ADL.Controllers
 {
@@ -32,7 +29,7 @@ namespace ADL.Controllers
             return View();
         }
         
-        public ViewResult Delete(int? locationId)
+        public ViewResult Delete(int? assignmentId)
         {
             return View();
         }
@@ -45,29 +42,5 @@ namespace ADL.Controllers
         {
             return View();
         }
-
-        public ViewResult Generate_QR(int locationID)
-        {
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(locationID.ToString(), QRCodeGenerator.ECCLevel.Q);
-            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
-
-            ViewData["QRCode"] = ByteArraytoURL(qrCode.GetGraphic(20)); ;
-            return View("ViewQR");
-        }
-
-        public string ByteArraytoURL(Byte[] qrCodeByteArray)
-        {
-            string imageBase64Data = Convert.ToBase64String(qrCodeByteArray);
-            string imageDataURL = string.Format("data:image/png;base64,{0}", imageBase64Data);
-
-            return imageDataURL;
-        }
-
-        public ViewResult TEST()
-        {
-            return View();
-        }
-
     }
 }
