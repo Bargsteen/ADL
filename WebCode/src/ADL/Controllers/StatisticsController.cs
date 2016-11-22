5c7cd7c4d8e17077cd680375a5132381804dd3a4
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2016-11-22 13:41:54 +0100

    Statistics has tables inside tables

--- WebCode/src/ADL/Controllers/StatisticsController.cs
+++ WebCode/src/ADL/Controllers/StatisticsController.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ADL.Models;
 using System;
+using ADL.Models;
+using ADL.Models.ViewModels;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ADL.Controllers
 {
     public class StatisticsController : Controller
     {
         private IAnswerRepository answerRepository;
-        public StatisticsController(IAnswerRepository answerRepo)
+        private IAssignmentRepository assignmentRepository;
+        public StatisticsController(IAssignmentRepository assignmentRepo, IAnswerRepository answerRepo)
         {
             answerRepository = answerRepo;
+            assignmentRepository = assignmentRepo;
         }
         public ViewResult Index()
         {
-            return View(answerRepository.Answers);
+            StatisticsViewModel statisticsViewModel = new StatisticsViewModel() 
+            { 
+                Answers = answerRepository.Answers, 
+                Assignments = assignmentRepository.Assignments 
+            };
+            
+            Dictionary<int, Tuple<int, int>> statsList = new Dictionary<int, Tuple<int, int>>();
+
+            foreach(Assignment assignment in statisticsViewModel.Assignments)
+            {
+                List<Answer> answersForThisAssignment = new List<Answer>();
+                foreach(Answer answer in statisticsViewModel.Answers)
+                {
+                    if(answer.AnsweredAssignmentId == assignment.AssignmentId)
+                    {
+                        answersForThisAssignment.Add(answer);
+                    }
+                }
+                Tuple<int, int> correctVsTotalAnswers = new Tuple<int, int>
+                (
+                    answersForThisAssignment.Where(a => a.ChosenAnswerOption == assignment.CorrectAnswer).Count(), // Count # of correct answers
+                    answersForThisAssignment.Count()
+                );
+                statsList.Add(assignment.AssignmentId, correctVsTotalAnswers);
+            }
+            statisticsViewModel.Stats = statsList;
+
+            return View(statisticsViewModel);
         }
     }
 }
