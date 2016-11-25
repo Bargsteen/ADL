1455bfd935809b88de706950f91129f0f3bfd3a8
Christian Larsen <clarse15@student.aau.dk>
2016-11-25 21:55:53 +0100

    Gjort klar til at tilføje skoler

---
+++ WebCode/src/ADL/Models/EFSchoolRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ADL.Models
+{
+    public class EFSchoolRepository : ISchoolRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFSchoolRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<School> Schools { get; }
+        
+        public void AddSchoolsToDb()
+        {
+            
+        }
+    }
+}
