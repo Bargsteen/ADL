06a0fead4f53b3a56934162b4b9ebf896a7981bf
ibrahim <kohistani_2@hotmail.com>
2016-11-28 13:54:14 +0100

    EFAnswer.Test

---
+++ WebCode/test/ADL.Tests3/EFAnswerRepTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using ADL.Models;
+
+namespace ADL.Tests
+{
+    public class EFAnswerRepTest
+    {
+        public void  { } 
+
+    }
+}
