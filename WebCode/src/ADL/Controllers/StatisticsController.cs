f850628a8e11e0f0c1217bc26f35e6273ad5c64b
Peter Madsen <p-printz@hotmail.com>
2016-12-03 14:46:55 +0100

    Added statistics according to new types
    Sends 3 different dict, to statistic index. Exclusive -> correct/total
    to each assignment. Multiple->list of percentage correct to each
    assignment(maybe make another viewmodel for multiple, so teacher can see
    what answeroptions etc)
    text -> list of strings to each assignment

--- WebCode/src/ADL/Controllers/StatisticsController.cs
+++ WebCode/src/ADL/Controllers/StatisticsController.cs
@@ -18,50 +18,88 @@ namespace ADL.Controllers
             answerRepository = answerRepo;
             assignmentSetRepository = assignmentSetRepo;
         }
-        //public ViewResult Index()
-        //{
-        //    List<Assignment> totalList = new List<Assignment>();
-        //    foreach (List<Assignment> assignmentList in assignmentSetRepository.AssignmentSets.Select(set => set.Assignments))
-        //    {
-        //        foreach (Assignment a in assignmentList)
-        //        {
-        //            totalList.Add(a);
-        //        }
-        //    }
-        //    IEnumerable<Assignment> asi = totalList;
-        //    StatisticsViewModel statisticsViewModel = new StatisticsViewModel()
-        //    {
-        //        Answers = answerRepository.Answers,
-        //        Assignments = asi,
-        //    };
 
-        //    Dictionary<int, Tuple<int, int>> exclusiveStatsList = new Dictionary<int, Tuple<int, int>>();
+        private IEnumerable<Answer> getAnswersForAssignment(Assignment assignment, IEnumerable<Answer> answers)
+        {
+            List<Answer> answersForThisAssignment = new List<Answer>();
+            foreach (Answer answer in answers)
+            {
+                if (answer.AnsweredAssignmentId == assignment.AssignmentId)
+                {
+                    answersForThisAssignment.Add(answer);
+                }
+            }
+            return answersForThisAssignment;
+        }
+        private void AddExclusiveStats(ExclusiveChoiceAssignment assignment, Dictionary<int, Tuple<int, int>> exclusiveStatsList)
+        {
+            List<Answer> answersForThisAssignment = getAnswersForAssignment(assignment,
+                answerRepository.Answers) as List<Answer>;
+            Tuple<int, int> correctVsTotalAnswers =
+                new Tuple<int, int>(answersForThisAssignment.Count(a => int.Parse(a.ChosenAnswers[0]) == assignment.CorrectAnswer), answersForThisAssignment.Count);
+            exclusiveStatsList.Add(assignment.AssignmentId, correctVsTotalAnswers);
+        }
 
-        //    foreach (Assignment assignment in statisticsViewModel.Assignments)
-        //    {
-        //        List<Answer> answersForThisAssignment = new List<Answer>();
-        //        foreach (Answer answer in statisticsViewModel.Answers)
-        //        {
-        //            if (answer.AnsweredAssignmentId == assignment.AssignmentId)
-        //            {
-        //                answersForThisAssignment.Add(answer);
-        //            }
-        //        }
-        //        if (assignment is ExclusiveChoiceAssignment)
-        //        {
-        //            Tuple<int, int> correctVsTotalAnswers = new Tuple<int, int>
-        //        (
+        private void AddMultipleStats(MultipleChoiceAssignment assignment, Dictionary<int, IEnumerable<double>> mStatsList)
+        {
+            List<double> answerPcts = new List<double>();
+            List<Answer> answersForThisAssignment = getAnswersForAssignment(assignment, answerRepository.Answers) as List<Answer>;
+            foreach (Answer a in answersForThisAssignment)
+            {
+                int correctAnswers = 0;
+                foreach (int correctAnswerIndex in assignment.CorrectAnswers)
+                {
+                    if (a.ChosenAnswers.Exists(ca => int.Parse(ca) == correctAnswerIndex))
+                        correctAnswers++;
+                }
+                answerPcts.Add((double)correctAnswers / assignment.AnswerOptions.Count);
+            }
+            mStatsList.Add(assignment.AssignmentId, answerPcts);
+        }
 
-        //            answersForThisAssignment.Where(a => a.ChosenAnswerOption == assignment.CorrectAnswer).Count(), // Count # of correct answers
-        //            answersForThisAssignment.Count()
-        //        );
-        //            exclusiveStatsList.Add(assignment.AssignmentId, correctVsTotalAnswers);
-        //        }
+        private IEnumerable<string> addTextualAnswers(Assignment assignment)
+        {
+            List<Answer> answersForThisAssignment = getAnswersForAssignment(assignment, answerRepository.Answers) as List<Answer>;
+            return answersForThisAssignment.Select(a => a.ChosenAnswers[0]).ToList();
+        }
 
-        //    }
-        //    statisticsViewModel.Stats = exclusiveStatsList;
+        public ViewResult Index()
+        {
+            List<Assignment> totalList = new List<Assignment>();
+            foreach (List<Assignment> assignmentList in assignmentSetRepository.AssignmentSets.Select(set => set.Assignments))
+            {
+                foreach (Assignment a in assignmentList)
+                {
+                    totalList.Add(a);
+                }
+            }
+            StatisticsViewModel statisticsViewModel = new StatisticsViewModel()
+            {
+                Answers = answerRepository.Answers,
+                AssignmentSets = assignmentSetRepository.AssignmentSets
+            };
+            foreach (Assignment assignment in totalList)
+            {
+                if (assignment is ExclusiveChoiceAssignment)
+                {
+                    Dictionary<int, Tuple<int, int>> exclusiveStatsList = new Dictionary<int, Tuple<int, int>>();
+                    AddExclusiveStats(assignment as ExclusiveChoiceAssignment, exclusiveStatsList);
+                    statisticsViewModel.ExclusiveStats = exclusiveStatsList;
+                }
 
-        //    return View(statisticsViewModel);
-        //}
+                else if (assignment is MultipleChoiceAssignment)
+                {
+                    Dictionary<int, IEnumerable<double>> mStatsList = new Dictionary<int, IEnumerable<double>>();
+                    AddMultipleStats(assignment as MultipleChoiceAssignment, mStatsList);
+                    statisticsViewModel.MultipleStats = mStatsList;
+                }
+
+                else
+                {
+                    statisticsViewModel.TextualAnswers.Add(assignment.AssignmentId, addTextualAnswers(assignment));
+                }
+            }
+            return View(statisticsViewModel);
+        }
     }
 }
--- WebCode/src/ADL/Models/ViewModels/StatisticsViewModel.cs
+++ WebCode/src/ADL/Models/ViewModels/StatisticsViewModel.cs
@@ -5,8 +5,11 @@ namespace ADL.Models.ViewModels
 {
     public class StatisticsViewModel
     {
-        public IEnumerable<Assignment> Assignments { get; set; }
+        public IEnumerable<AssignmentSet> AssignmentSets { get; set; }
         public IEnumerable<Answer> Answers { get; set; }
-        public Dictionary<int,Tuple<int,int>> Stats { get; set; }
+        public Dictionary<int,Tuple<int,int>> ExclusiveStats { get; set; }
+        public Dictionary<int, IEnumerable<double>> MultipleStats { get; set; }
+        public Dictionary<int, IEnumerable<string>> TextualAnswers { get; set; }
+
     }
 }
