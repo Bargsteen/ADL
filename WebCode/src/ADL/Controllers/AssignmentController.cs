71fb01da3c1063cb85e03b5076987aaa486f0739
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2016-12-04 18:36:26 +0100

    AssignmentSetEdit view works

--- WebCode/src/ADL/Controllers/AssignmentController.cs
+++ WebCode/src/ADL/Controllers/AssignmentController.cs
@@ -26,23 +26,6 @@ namespace ADL.Controllers
             userManager = usrMgr;
         }
 
-
-
-        public ViewResult StudentPick()
-        {
-            PersonAndAssignmentViewModel studentList = new PersonAndAssignmentViewModel()
-            {
-                /*TEST ANDREAS*/
-                AssignmentSets = new List<AssignmentSet>() { new AssignmentSet(), new AssignmentSet() },
-
-                Persons = new List<Person>() { new Person(), new Person(), new Person(), new Person(), new Person() }
-                
-                //AssignmentSets = assignmentSetRepository.AssignmentSets,
-                //Persons = userManager.Users as IEnumerable<Person>
-            };
-            return View(studentList);
-        }
-
         public ViewResult List()
         {
             AssignmentAndLocationListViewModel assignmentList = new AssignmentAndLocationListViewModel()
@@ -84,7 +67,7 @@ namespace ADL.Controllers
             return View(model);
         }
 
-        // Uses the edit view, but gives it a new assignment. j
+        // Uses the edit view, but gives it a new assignment.
         public async Task<ViewResult> Create()
         {
             Person currentUser = await GetCurrentUserAsync();
@@ -113,32 +96,5 @@ namespace ADL.Controllers
             }
             return RedirectToAction(nameof(List));
         }
-
-        public ViewResult AttachAssignmentToLocation(int chosenAssignmentId)
-        {
-            AssignmentToLocationAttachment attachment = new AssignmentToLocationAttachment();
-            attachment.ChosenAssignmentId = chosenAssignmentId;
-            attachment.Locations = locationRepository.Locations;
-            return View(attachment);
-        }
-
-        /*[HttpPost]
-        public async Task<IActionResult> AttachAssignmentToLocation(AssignmentToLocationAttachment attachment)
-        {
-            Location chosenLocation = locationRepository.Locations.FirstOrDefault(l => l.LocationId == attachment.ChosenLocationId);
-            Assignment chosenAssignment = assignmentSetRepository.AssignmentSets.FirstOrDefault(b => b.AssignmentSetId == attachment.ChosenAssignmentSetId).Assignments.FirstOrDefault(a => a.AssignmentId == attachment.ChosenAssignmentId);
-            Person chosenPerson = await GetCurrentUserAsync();
-            if (chosenLocation != null && chosenAssignment != null)
-            {
-                locationRepository.SaveAttachedAssignmentId(chosenLocation.LocationId, chosenPerson.Id, chosenAssignment.AssignmentId);
-                TempData["message"] = $"Opgaven '{chosenAssignment.Title}' blev koblet med lokationen '{chosenLocation.Title}'";
-                return RedirectToAction(nameof(List));
-            }
-            return View(attachment);
-        }
-*/
-
-
-
     }
 }
