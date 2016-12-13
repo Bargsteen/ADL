eccf50a332d83383c7bc0cbf230678d8fe74a640
Peter Madsen <p-printz@hotmail.com>
2016-12-13 16:35:08 +0100

    started on couplingcontrollertests

--- WebCode/test/ADL.Tests/CouplingControllerTests.cs
+++ WebCode/test/ADL.Tests/CouplingControllerTests.cs
@@ -12,6 +12,7 @@ using ADL.Models.Assignments;
 using ADL.Models.Repositories;
 using Xunit;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Xunit.Sdk;
@@ -23,13 +24,22 @@ namespace ADL.Tests
         private Mock<IAssignmentSetRepository> mockAssignmentSetRepository = new Mock<IAssignmentSetRepository>();
         private Mock<ILocationRepository> mockLocationRepository = new Mock<ILocationRepository>();
         private Mock<IClassRepository> mockClassRepository = new Mock<IClassRepository>();
+        private Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
         private readonly CouplingController _couplingController;
         public CouplingControllerTests()
         {
             mockAssignmentSetRepository = new Mock<IAssignmentSetRepository>();
             mockAssignmentSetRepository.Setup(ma => ma.AssignmentSets).Returns(new[]
+            {
+                new AssignmentSet {
+                    AssignmentSetId = 1, Title = "TestTitle", Description = "TestDescription", Assignments = new List<Assignment>()
+                    {
+                        new Assignment
                         {
-                new AssignmentSet {AssignmentSetId = 1, Title = "TestTitle", Description = "TestDescription", Assignments = new List<Assignment>(){new Assignment { AssignmentId = 7, Text = "TestText"}}  }
+                            AssignmentId = 7, Text = "TestText"
+                        }
+                    }
+                }
              });
             mockLocationRepository = new Mock<ILocationRepository>();
             mockLocationRepository.SetupGet(ml => ml.Locations).Returns(new List<Location>()
@@ -38,41 +48,18 @@ namespace ADL.Tests
                     {
                         Description = "TestDescription",
                         LocationId = 30, SchoolId = 42,
-                        PersonAssignmentCouplings = new List<PersonAssignmentCoupling>() {
-                            new PersonAssignmentCoupling()
-                        {
-                            AssignmentId = 7, PersonAssignmentCouplingId = 2, PersonId = "TestPerId"
-
-                        }
-                        },
                         Title = "TestTitle"
                     },
                     new Location()
                     {
-                         Description = "TestDescription2",
+                        Description = "TestDescription2",
                         LocationId = 31, SchoolId = 42,
-                        PersonAssignmentCouplings = new List<PersonAssignmentCoupling>() {
-                            new PersonAssignmentCoupling()
-                        {
-                            AssignmentId = 7, PersonAssignmentCouplingId = 2, PersonId = "TestPerId2"
-                        },
-                            new PersonAssignmentCoupling()
-                        {
-                            AssignmentId = 7, PersonAssignmentCouplingId = 3, PersonId = "TestPerId"
-                        }
-                        },
                         Title = "TestTitle2"
                     },
                     new Location()
                     {
                         Description = "TestDescription",
                         LocationId = 329, SchoolId = 42,
-                        PersonAssignmentCouplings = new List<PersonAssignmentCoupling>() {
-                            new PersonAssignmentCoupling()
-                        {
-                            AssignmentId = 7, PersonAssignmentCouplingId = 2, PersonId = "TestPerId2"
-                        }
-                        },
                         Title = "TestTitle"
                     }
             });
@@ -100,9 +87,76 @@ namespace ADL.Tests
                             Firstname = "TestFN4", Lastname = "TestLN4", Id = "TestId4", SchoolId = 1
                         },
                     }
-                }, new Class()
+                },
+                new Class()
                 {
                     ClassId = 2, SchoolId = 1, Name = "Test2", StartYear = 2005, People = new List<Person>()
+                    {
+                        new Person()
+                        {
+                            Firstname = "TestFN1", Lastname = "TestLN1", Id = "TestId1", SchoolId = 1, PersonType = EnumCollection.PersonTypes.Teacher
+                        },
+                         new Person()
+                        {
+                            Firstname = "TestFN2", Lastname = "TestLN2", Id = "TestId2", SchoolId = 1, PersonType = EnumCollection.PersonTypes.Student
+                        },
+                          new Person()
+                        {
+                            Firstname = "TestFN3", Lastname = "TestLN3", Id = "TestId3", SchoolId = 1, PersonType = EnumCollection.PersonTypes.Student
+                        },
+                           new Person()
+                        {
+                            Firstname = "TestFN4", Lastname = "TestLN4", Id = "TestId4", SchoolId = 1, PersonType = EnumCollection.PersonTypes.Student
+                        },
+                    }
+                },
+                new Class()
+                {
+                    ClassId = 3, SchoolId = 2, Name = "Test2", StartYear = 2005, People = new List<Person>()
+                    {
+                        new Person()
+                        {
+                            Firstname = "TestFN1", Lastname = "TestLN1", Id = "TestId1", SchoolId = 1
+                        },
+                         new Person()
+                        {
+                            Firstname = "TestFN2", Lastname = "TestLN2", Id = "TestId2", SchoolId = 1
+                        },
+                          new Person()
+                        {
+                            Firstname = "TestFN3", Lastname = "TestLN3", Id = "TestId3", SchoolId = 1
+                        },
+                           new Person()
+                        {
+                            Firstname = "TestFN4", Lastname = "TestLN4", Id = "TestId4", SchoolId = 1
+                        },
+                    }
+                },
+                new Class()
+                {
+                    ClassId = 5, SchoolId = 2, Name = "Test2", StartYear = 2005, People = new List<Person>()
+                    {
+                        new Person()
+                        {
+                            Firstname = "TestFN1", Lastname = "TestLN1", Id = "TestId1", SchoolId = 1
+                        },
+                         new Person()
+                        {
+                            Firstname = "TestFN2", Lastname = "TestLN2", Id = "TestId2", SchoolId = 1
+                        },
+                          new Person()
+                        {
+                            Firstname = "TestFN3", Lastname = "TestLN3", Id = "TestId3", SchoolId = 1
+                        },
+                           new Person()
+                        {
+                            Firstname = "TestFN4", Lastname = "TestLN4", Id = "TestId4", SchoolId = 1
+                        },
+                    }
+                },
+                new Class()
+                {
+                    ClassId = 4, SchoolId = 2, Name = "Test2", StartYear = 2005, People = new List<Person>()
                     {
                         new Person()
                         {
@@ -123,17 +177,45 @@ namespace ADL.Tests
                     }
                 }
             });
-            _couplingController = new CouplingController(mockLocationRepository.Object, mockClassRepository.Object, mockAssignmentSetRepository.Object);
+            _couplingController = new CouplingController(mockLocationRepository.Object, mockClassRepository.Object, mockAssignmentSetRepository.Object) { TempData = tempData.Object };
         }
         [Fact]
-        public void TestChooseClass()
+        public void TestChooseClassForAvailableClassesIsCorrect()
         {
             //Act
-            var result = _couplingController.ChooseClass(1, 1);
+            var result1 = _couplingController.ChooseClass(1, 1);
+            bool isClassesAppropriateAccordingToSchoolId1 =
+                            (result1.Model as ChooseClassViewModel).AvailableClasses.All(c => c.SchoolId == 1);
             //Assert
-            bool isClassesAppropriateAccordingToSchoolId =
-                (result.Model as ChooseClassViewModel).AvailableClasses.All(c => c.SchoolId == 1);
-            Assert.True(isClassesAppropriateAccordingToSchoolId);
+            Assert.True(isClassesAppropriateAccordingToSchoolId1);
+            Assert.Equal(1, (result1.Model as ChooseClassViewModel).ChosenAssignmentSetId);
+
+            var result2 = _couplingController.ChooseClass(42, 1);
+
+            Assert.Empty((result2.Model as ChooseClassViewModel).AvailableClasses);
+            Assert.Equal(1, (result2.Model as ChooseClassViewModel).ChosenAssignmentSetId);
+
+            var result3 = _couplingController.ChooseClass(2, 1);
+            bool isClassesAppropriateAccordingToSchoolId2 =
+                (result3.Model as ChooseClassViewModel).AvailableClasses.All(c => c.SchoolId == 2);
+
+            Assert.True(isClassesAppropriateAccordingToSchoolId2);
+            Assert.Equal(1, (result3.Model as ChooseClassViewModel).ChosenAssignmentSetId);
+        }
+
+        private DifferentiateViewModel dvm;
+        [Fact]
+        public void TestDifferentiate()
+        {
+            var result = _couplingController.Differentiate(1, 2);
+            //Number of people connected to class 2 is 4, tests if differentiate gets students only
+            Assert.Equal(3, (result.Model as DifferentiateViewModel).ChosenClass.People.Count);
+            int chosenSchoolId = (result.Model as DifferentiateViewModel).ChosenClass.SchoolId;
+            Assert.Equal(chosenSchoolId, (result.Model as DifferentiateViewModel).CurrentSchoolId);
+            Assert.Equal(1, (result.Model as DifferentiateViewModel).ChosenAssignmentSet.AssignmentSetId);
+            //Instantiates dvm with result of calling Differentiate with chosenAssignmentSetId and chosenClass
+            dvm = result.Model as DifferentiateViewModel;
+
         }
     }
 }
